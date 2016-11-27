d4b581eb60cc2d4a7077c7a495f40d2e618685a7
winio94 <winio94@gmail.com>
2016-11-27 23:28:36 +0100

    create spring boot main class

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,8 +1,7 @@
+package com;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Arrays;
 
 /**
  * Created by Michał on 2016-11-27.
@@ -11,14 +10,6 @@ import java.util.Arrays;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        SpringApplication.run(Application.class, args);
     }
 }
