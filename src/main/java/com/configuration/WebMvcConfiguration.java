4eb4cdc41ebb46d31031821e9603419bc3a304d4
winio94 <winio94@gmail.com>
2016-12-01 22:03:42 +0100

    add message source bean to config

--- src/main/java/com/configuration/WebMvcConfiguration.java
+++ src/main/java/com/configuration/WebMvcConfiguration.java
@@ -1,7 +1,9 @@
 package com.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -23,6 +25,13 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
         resolver.setPrefix("/WEB-INF/pages/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
     }
 
     @Override
